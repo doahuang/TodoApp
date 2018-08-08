a1f82a3cc54b646dc9fae180f570abf6934186c3
doahuang <doahuang@gmail.com>
2018-08-07 22:52:14 -0700

    Added todo list and container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoListContainer from './todo_list/todo_list_container';
 
 const App = () => {
   return (
     <div>
       <h1>Todo List</h1>
+      <TodoListContainer />
     </div>
   );
 };
---
+++ frontend/components/todo_list/todo_list.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h3>List here</h3>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
